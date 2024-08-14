fb65de53de6bebe6d0ddd51691c16c7b6fb4e261
Develku <110651468+Develku@users.noreply.github.com>
2024-08-14 10:32:33 +1000

    color theme update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,17 +8,26 @@ return {
   --   vim.cmd([[colorscheme gruvbox]])
 	--end,
   --
-  -- catppuccino colorscheme
   -- catppuccin colorscheme
-  "catppuccin/nvim",
-  name = "catppuccin",
+  -- "catppuccin/nvim",
+  -- name = "catppuccin",
+  -- priority = 1000,
+  -- lazy = false, -- Do NOT load the lazy
+  -- config = function()
+  --   require("catppuccin").setup({
+  --     flavour = "latte", -- Options: "latte" (light), "frappe" (medium contrast), "macchiato" (darker), "mocha" (darkest)
+  --     -- You can customize other settings here
+  --   })
+  --   vim.cmd([[colorscheme catppuccin]])
+  -- end,
+  --
+  -- github colorscheme
+  "projekt0n/github-nvim-theme",
+  lazy = false, -- Do NOT load the lazy colorscheme
   priority = 1000,
-  lazy = false, -- Do NOT load the lazy
   config = function()
-    require("catppuccin").setup({
-      flavour = "latte", -- Options: "latte" (light), "frappe" (medium contrast), "macchiato" (darker), "mocha" (darkest)
-      -- You can customize other settings here
+    require('github-theme').setup({
     })
-    vim.cmd([[colorscheme catppuccin]])
+    vim.cmd('colorscheme github_light_default')
   end,
 }
